f3d12d6cc1c577542372f6e5900617eb6cf49749
Kelby Behounek <knbehounek@hotmail.com>
2018-06-03 12:39:41 -0700

    v1

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//Array of topics, allow new topcs to be pushed into this array
+// Create a function that displays all gif buttons
+// Create a function to add a new topic button
+// Create a function to remove last topic button
+// Create a function that displays all of the gif
+// Call the functions & methods
