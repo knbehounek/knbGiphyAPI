86f0e2b678b099a6e801a027914c2eeabd4c271d
Kelby Behounek <knbehounek@hotmail.com>
2018-06-03 17:36:13 -0700

    v1.1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
+$( document ).ready(function() {
 //Array of topics, allow new topcs to be pushed into this array
+var topic = ['Dragons', 'Games', 'Eating' ]
 // Create a function that displays all gif buttons
+function displayGifButtons(){
+    $("#gifView").empty(); 
+    for (var i = 0; i < topic.length; i++){
+        var gifButton = $("<button>");
+        gifButton.addClass("topic");
+        gifButton.addClass("btn btn-primary")
+        gifButton.attr("data-name", topic[i]);
+        gifButton.text(topic[i]);
+        $("#gifView").append(gifButton);
+    }
+}
+
 // Create a function to add a new topic button
 // Create a function to remove last topic button
 // Create a function that displays all of the gif
-// Call the functions & methods
+// Call the functions & Methods 
