754d1b078336996e440f3c6bcffc11fdd6ae8a27
Kelby Behounek <knbehounek@hotmail.com>
2018-06-04 15:07:26 -0700

    v1.3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,70 @@
 $( document ).ready(function() {
 //Array of topics, allow new topcs to be pushed into this array
-var topic = ['Dragons', 'Games', 'Eating' ]
+var topics = ['Dragons', 'Games', 'Eating' ]
+
 // Create a function that displays all gif buttons
 function displayGifButtons(){
-    $("#gifView").empty(); 
-    for (var i = 0; i < topic.length; i++){
+    $("#gifButton").empty(); 
+    for (var i = 0; i < topics.length; i++){
         var gifButton = $("<button>");
         gifButton.addClass("topic");
         gifButton.addClass("btn btn-primary")
-        gifButton.attr("data-name", topic[i]);
-        gifButton.text(topic[i]);
-        $("#gifView").append(gifButton);
+        gifButton.attr("data-name", topics[i]);
+        gifButton.text(topics[i]);
+        $("#gifButton").append(gifButton);
     }
 }
 
 // Create a function to add a new topic button
-// Create a function to remove last topic button
-// Create a function that displays all of the gif
+
+function newButton () {
+    $("#addGif").on("click", function(){
+        var topic = $("#topic-input").val().trim();
+    if (topic == ""){
+        return false;
+    }
+    topics.push(topic);
+
+    displayGifButtons();
+    return false;
+    });
+    }
+// Create a function to Remake topic buttons
+// Create a function that displays all of the gifs
+function displayGif(){
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=NH1i7GZqCBF337lj1ljpGN4BzKCNmzX8=10";
+    console.log(queryURL); 
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    .done(function(response){
+        $("#gifsview").empty();
+        var results = response.data
+        if (results == "") {
+            alert("There is not gif for button")
+        }
+    for ( var i = 0; i<results.length; i++){
+        
+        var gifDiv = $("<div>");
+        gifDiv.addClass("gifDiv");
+        var gifRating =$("<p>").text("Rating: " + results[i].rating)
+        gifDiv.append(gifRating);
+        var gifImg = $("<img>")
+        gifImg.attr("src", results[i].images.fixed_height_small_still.url); 
+        gifImg.attr("data-still",results[i].images.fixed_height_small_still.url); 
+        gifImg.attr("data-animate",results[i].images.fixed_height_small.url); 
+        gifImg.attr("data-state", "still"); 
+        gifImg.addClass("image");
+        gifDiv.append(gifImg);
+        $("#gifsView").prepend(gifDiv);
+    }
+    })
+}
 // Call the functions & Methods 
+
+displayGifButtons();
+newButton();
+displayGif();
+}) 
